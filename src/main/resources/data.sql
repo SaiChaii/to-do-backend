8b883036ed5457252384e3970856792ae842984f
Chaitanya <your_email@example.com>
2025-03-16 00:00:39 +0530

    added pandya

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
 INSERT INTO employee (f_Name, l_Name, age,phone,email_Id)
 VALUES ('MS', 'Dhoni',40, 9999999990, 'msd7@gmail.com'),
-       ('Virat', 'Kohli', 7897897898,'vk18@gmail.com');
+       ('Virat', 'Kohli', 7897897898,'vk18@gmail.com'),
+       ('Pandya', 'Hardik', 7897097898,'vkpu8@gmail.com'),;
